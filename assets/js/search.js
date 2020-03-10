7eb8aafce539d20a1a34346540067a8db28de933
sshleifer <sshleifer@users.noreply.github.com>
2020-03-10 20:10:01 +0000

    deploy: 428a0a84eff3ccac084033d38ae8d33378bf8266

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/tensorgoose/assets/js/search-data.json', true);
+    request.open('GET', '/blog_v2/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
